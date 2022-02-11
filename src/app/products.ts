09fc47bd73fd8b92df9575510e27b0066a8bf0b2
gapolayac <gapolayac@gmail.com>
2022-02-11 10:10:55 +0100

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -23,6 +23,12 @@ export const products = [
     name: 'Phone Standard',
     price: 299,
     description: ''
+  },
+  {
+    id: 4,
+    name: 'Phone Personal',
+    price: 199,
+    description: 'Una pasada'
   }
 ];
 
